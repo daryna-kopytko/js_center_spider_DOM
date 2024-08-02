24297266e5689b58c9b27d1232618321bc3a46ff
Daryna Kopytko <dashindafox@gmail.com>
2024-08-02 18:45:03 +0300

    make build

---
+++ index.8c1855bc.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=(t.clientHeight+t.clientWidth)/2,l=(e.clientHeight+e.clientWidth)/2;e.style.top="".concat((c-l)/2,"px"),e.style.left="".concat((c-l)/2,"px");
+//# sourceMappingURL=index.8c1855bc.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const wallSize = (wall.clientHeight + wall.clientWidth) / 2;
+const spiderSize = (spider.clientHeight + spider.clientWidth) / 2;
+const positionTop = (wallSize - spiderSize) / 2;
+const positionLeft = (wallSize - spiderSize) / 2;
+spider.style.top = `${positionTop}px`;
+spider.style.left = `${positionLeft}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
